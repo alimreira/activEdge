95a5776db09323ea0a4722cbf0d00a3f0c8dd81e
AlimReira <feyikemipereira1@gmail.com>
2021-11-25 12:05:42 +0100

    started Spring boot application and created entity

---
+++ src/main/java/com/assessment/activedge/entity/Stock.java
@@ -0,0 +1,66 @@
+package com.assessment.activedge.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double current_price;
+    private Date create_date;
+    private Date last_update;
+
+    public Stock(Long id) {
+        this.id = id;
+    }
+
+    public Stock() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCurrent_price() {
+        return current_price;
+    }
+
+    public void setCurrent_price(Double current_price) {
+        this.current_price = current_price;
+    }
+
+    public Date getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(Date create_date) {
+        this.create_date = create_date;
+    }
+
+    public Date getLast_update() {
+        return last_update;
+    }
+
+    public void setLast_update(Date last_update) {
+        this.last_update = last_update;
+    }
+}
---
+++ src/test/java/com/assessment/activedge/ActivEdgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.activedge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivEdgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
